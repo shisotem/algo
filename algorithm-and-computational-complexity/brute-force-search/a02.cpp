698b991fa223db3792b8a5b1a0eafb9f8321f8c7
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-27 22:16:40 +0900

    feat: linear search

---
+++ algorithm-and-computational-complexity/brute-force-search/a02.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+  int N, X, A[100];
+  cin >> N >> X;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  bool exist = false;
+  for (int i = 0; i < N; i++) {
+    if (A[i] == X) {
+      exist = true;
+      break;
+    }
+  }
+
+  if (exist) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
