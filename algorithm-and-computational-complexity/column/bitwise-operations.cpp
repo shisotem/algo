987667b88a13480b64abe4ef16c8522cf30b9cee
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-28 03:05:59 +0900

    feat: bitwise operations

---
+++ algorithm-and-computational-complexity/column/bitwise-operations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  cout << "a & b = " << (a & b) << endl;
+  cout << "a | b = " << (a | b) << endl;
+  cout << "a ^ b = " << (a ^ b) << endl;
+  // cout << "~a = " << (~a) << endl;
+  // cout << "a << 1 = " << (a << 1) << endl;
+  // cout << "a >> 1 = " << (a >> 1) << endl;  
+  return 0;
+}
