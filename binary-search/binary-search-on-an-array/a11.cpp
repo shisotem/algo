6c77360cdfa1aa0bb4d610573e66adb851c1bdc2
shisotem <129283841+shisotem@users.noreply.github.com>
2023-10-29 11:55:56 +0900

    feat: binary search

---
+++ binary-search/binary-search-on-an-array/a11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int A[], int N, int X) {
+  int left = 0, right = N - 1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (A[mid] == X) return mid;
+    if (A[mid] > X) right = mid - 1;
+    if (A[mid] < X) left = mid + 1;
+  }
+  return -1; // not found
+}
+
+int main() {
+  int N, X, A[100000];
+  cin >> N >> X;
+  for (int i = 0; i < N; i++) cin >> A[i];
+
+  int ans = binary_search(A, N, X);
+  cout << ans << endl;
+  return 0;
+}
