46c24ede4271baa1df2690f356c32d017c1a8b50
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-13 13:52:01 +0900

    feat(#11): complete task1

--- 2a/11/main.java
+++ 2a/11/main.java
@@ -1,31 +1,45 @@
 // 必要なものはimportしてください．
+import java.io.*;
 
 class main {
 	public static void main(String[] args) {
 		int idx;
 
 // 課題1 text.txtとpattern.txt内の文字列を読み込んで，それぞれStirng型のtxtとpatに格納する
-
+  	String txt = "";
+    String pat = "";
+    try {
+      BufferedReader txtReader = new BufferedReader(new FileReader("text.txt"));
+  	 	BufferedReader patReader = new BufferedReader(new FileReader("pattern.txt"));
+      txt = txtReader.readLine();
+      pat = patReader.readLine();
+      txtReader.close();
+      patReader.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    // System.out.println("txt: " + txt);
+    // System.out.println("pat: " + pat);
 
 
 // 課題2 力まかせ法 文字列s1から文字列s2を探索
-		BFmatch b = new BFmatch();
-		idx = b.bfmatch(txt, pat);
-		b.printBF();
+		// BFmatch b = new BFmatch();
+		// idx = b.bfmatch(txt, pat);
+		// b.printBF();
 // 課題2 力まかせ法 文字列s1から文字列s2を探索 (ここまで)
 
 // 課題2を取り組む際は以下はコメントアウト
 // 課題3 KMP法 文字列s1から文字列s2を探索
-		KMPmatch k = new KMPmatch();
-		idx = k.kmpmatch(txt, pat);
-		k.printKMP();
+		// KMPmatch k = new KMPmatch();
+		// idx = k.kmpmatch(txt, pat);
+		// k.printKMP();
 // 課題3 KMP法 文字列s1から文字列s2を探索 (ここまで)
 
 // 課題2・3を取り組む際は以下はコメントアウト
 // 課題4 BM法 文字列s1から文字列s2を探索
-		BMmatch bm = new BMmatch();
-		idx = bm.bmmatch(txt, pat);
-		bm.printBM();
+		// BMmatch bm = new BMmatch();
+		// idx = bm.bmmatch(txt, pat);
+		// bm.printBM();
 // 課題4 BM法 文字列s1から文字列s2を探索 (ここまで)
 
 
