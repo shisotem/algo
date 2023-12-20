9cfecb10722ae6127a125871fbc4799a3ce522f2
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-20 13:41:35 +0900

    feat(#12): complete task1

--- 2a/12/SUM.java
+++ 2a/12/SUM.java
@@ -6,10 +6,17 @@ class SUM {
 // 課題1
     int sum(int n) { // "nまでの和"を求める．
 		// 編集: 次を出力 [ 関数sum(n)を呼び出しました．]; nの部分は，int nの数字を入れる
+		System.out.println("関数sum(" + n + ")を呼び出しました．");
 		// 編集: base caseで，n=0で戻り値が0
+		if (n == 0) {
+        return 0;
+    }
 		// 編集: 結果resultは，n+sum(n-1) として再帰的にsumを呼び出す
+		int result = n + sum(n - 1);
 		// 編集: 次を出力 [ nまでの和 = result ]; nの部分は，int nの数字を入れる
+		System.out.println(n + "までの和 = " + result);
 		// 編集: 戻り値はresult
+		return result;
     }
 // 課題1　(ここまで)
 
