1a7ebed901c0ef466d8587406bdcb7773c922392
shisotem <129283841+shisotem@users.noreply.github.com>
2024-01-11 20:05:44 +0900

    feat(#13): complete Knapsack

--- 2a/13/Knapsack.java
+++ 2a/13/Knapsack.java
@@ -8,18 +8,30 @@ class Knapsack {
 	static void knapSack(int W, ArrayList<Integer>  wt, ArrayList<Integer>  val) 
 	// 変数　W: ナップザックの制限重量, wt: 荷物の重さの配列, val: 荷物の価値の配列
 	{
+				T = new ArrayList<>();
+        int n = wt.size();
+        int[][] dp = new int[n+1][W+1];
 
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt.get(i-1) <= w)
+                    dp[i][w] = Math.max(val.get(i-1) + dp[i-1][w-wt.get(i-1)], dp[i-1][w]);
+                else
+                    dp[i][w] = dp[i-1][w];
+            }
+        }
 
+        value = dp[n][W];
 
-
-
-
-
-
-
-
-		
+        int w = W;
+        for (int i = n; i > 0 && w > 0; i--) {
+            if (dp[i][w] != dp[i-1][w]) {
+                T.add(i);
+                w = w - wt.get(i-1);
+            }
+        }
 	}
-
 }
 
--- 2a/13/Main_Knapsack.java
+++ 2a/13/Main_Knapsack.java
@@ -1,13 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 class Main_Knapsack {
 	public static void main(String args[])
 	{
+        ArrayList<Integer> wt = new ArrayList<>();
+        ArrayList<Integer> val = new ArrayList<>();
 
+        try {
+            Scanner data = new Scanner(new File("data.txt"));
+            while (data.hasNextLine()) {
+                String[] line = data.nextLine().split(", ");
+                wt.add(Integer.parseInt(line[0]));
+                val.add(Integer.parseInt(line[1]));
+            }
+            data.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("ナップザックの制限重量 = ");
+        int W = input.nextInt();
+        input.close();
 
+        Knapsack.knapSack(W, wt, val);
 
-		
+				Collections.reverse(Knapsack.T);
 
+				String items = Knapsack.T.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining(", "));
 
+        System.out.println("荷物の種類: " + items);
+        System.out.println("荷物の総重量 = " + W);
+        System.out.println("荷物の価値の総和 = " + Knapsack.value);
 	}
 }
